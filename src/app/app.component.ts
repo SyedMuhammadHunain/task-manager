a144dc95b0be83c130ab6bb254c0c045ba312e62
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-07-09 10:44:28 +0500

    show task of clicked user (only name show)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  foundUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.foundUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.foundUserId = id;
+  }
 }
