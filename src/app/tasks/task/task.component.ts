ab92f965880ea8e3e7dc1d4c4fc6cf7f492a7f67
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-07-12 22:14:48 +0500

    Added good practises

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,20 +1,22 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import {  Task } from './task.model';
-import { CardComponent } from "../../shared/card/card.component"; 
+import { Component, Input, inject } from '@angular/core';
+import { Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
 import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-in-task',
   standalone: true,
   imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+
+  private tasksService = inject(TasksService);
 
   onComplete() {
-    this.complete.emit(this.task.id);
+    this.tasksService.deleteTask(this.task.id);
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { NewTask } from './new-task/new-task.model';
 import { TasksService } from './tasks.service';
+
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -14,18 +15,13 @@ export class TasksComponent {
   isAddTask: boolean = false;
   @Input() userId!: string;
   @Input() name!: string;
-  tasksService: TasksService;
 
-  constructor(tasksService: TasksService) {
-    this.tasksService = tasksService;
-  }
+  private tasksService = inject(TasksService);
 
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string) {
-  }
   onAddTask() {
     this.isAddTask = true;
   }
