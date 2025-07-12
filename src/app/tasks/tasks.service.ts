b82991ddbb487e80788fafb6887f7687149d5db9
Syed Muhammad Hunain <hunainsince2005@gmail.com>
2025-07-12 23:18:31 +0500

    Added LocalStorage & Finalized

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -29,6 +29,13 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -41,9 +48,15 @@ export class TasksService {
       summary: newTask.summary,
       dueDate: newTask.dueDate,
     });
+    this.saveTasks();
   }
 
   deleteTask(id: string) {
     return (this.tasks = this.tasks.filter((task) => task.id !== id));
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
